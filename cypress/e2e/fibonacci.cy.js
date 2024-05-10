7cb3028f12e859a029c67b13e85eab015ae36694
VeraDgt <veradgt@gmail.com>
2024-05-10 14:43:53 +0300

    feat/fibonacci test

---
+++ cypress/e2e/fibonacci.cy.js
@@ -0,0 +1,35 @@
+import { URL, elStateColors } from "../../src/constants/constants";
+// import { testStringReverse } from "./utils";
+import { SHORT_DELAY_IN_MS } from "../../src/constants/delays";
+import { testCircle } from "../../src/constants/constants";
+
+
+describe('Фибоначчи: проверка компонентов', function() {
+  beforeEach(function() {
+    cy.visit(`${URL}/fibonacci`);
+  });
+
+  it('Если в инпуте пусто, то кнопка добавления недоступна', function() {
+    cy.contains("Фибоначчи");
+    cy.get("input").should("be.empty");
+    cy.get("button").last().should("be.disabled");
+  });
+
+  it("Проверка корректности генерации чисел", function () {
+      const inputValue = "7";
+      const fibArr = [1 , 1, 2, 3, 5, 8, 13, 21];
+
+      cy.get("input").as("currInput");
+      cy.get("@currInput").type(inputValue);
+
+      cy.get("button").contains("Рассчитать").as("currButton");
+      cy.get("@currButton").contains("Рассчитать").click();
+
+      cy.get(testCircle, {timeout: SHORT_DELAY_IN_MS * (fibArr.length + 1)})
+        .as("circles")
+        .should("have.length", fibArr.length)
+        .each((item, index) => {
+          expect(item).to.contain(`${fibArr[index]}`)
+      });
+  });
+});
--- cypress/e2e/utils.js
+++
@@ -1,9 +0,0 @@
-import { testCircle } from "../../src/constants/constants";
-
-// export const testStringReverse = (value, color) => {
-//   cy.get(testCircle).each(($el, index) => {
-//     cy.get($el)
-//       .should("have.css", "border-color", value[index])
-//       .should("contain", color[index]);
-//   });
-// };
